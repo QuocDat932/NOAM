9974b01557a0db7f7a405f6258ed89bfb0e7d4bb
Khoi Nguyen <khoinguynn2005@gmail.com>
2026-06-08 15:52:45 +0700

    Add thông tin sản phẩm JSL003 new

--- data/data_lac_tay.js
+++ data/data_lac_tay.js
@@ -1,3 +1,14 @@
 const data_lac_tay = [
     // ── JEWELRY: Lắc Tay Nam ──
+    {
+        id: 67, code: 'JSL003', catalogId: 'jewelry',
+        tags: { line: 'bac', type: 'lac-tay' }, isNew: false, inStock: true,
+        name: { vi: 'Lắc tay mắt xích', en: 'Link bracelet' },
+        image: './Img/Lac-tay-nu/JSL003/JSL003_01.png',
+        image1: './Img/Lac-tay-nu/JSL003/JSL003_02.png',
+        description: {
+            vi: '- Phối: Đá CZ\n- Chất liệu: Bạc xi bạch kim\nCâu chuyện sản phẩm: Thiết kế mắt xích sang trọng cùng đá CZ sáng đẹp, giúp hoàn thiện vẻ ngoài tinh tế và cuốn hút.',
+            en: '- Accents: CZ\n- Material: Platinum-plated silver\nProduct story: Elegant link design with bright and beautiful CZ stones, completing a refined and attractive look.'
+        }
+    },
 ];
